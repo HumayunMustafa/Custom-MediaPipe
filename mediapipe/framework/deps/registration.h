9bb34da01de2d3d260d50f20aa3439f2abfbde38
HumayunMustafa <Humayun@msd-my.com>
2025-06-12 17:44:50 +0500

    update: working on registration header in framework->deps

---
+++ mediapipe/framework/deps/registration.h
@@ -0,0 +1,32 @@
+//
+// Created by MSD on 12/06/2025.
+//
+
+#ifndef MEDIAPIPE_DEPS_REGISTRATION_H_
+#define MEDIAPIPE_DEPS_REGISTRATION_H_
+
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+
+#include "absl/base/macros.h"
+#include "absl/base/thread_annotations.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/log/absl_check.h"
+#include "absl/log/absl_log.h"
+#include "absl/meta/type_traits.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "absl/strings/string_view.h"
+#include "absl/synchronization/mutex.h"
+#include "mediapipe/framework/deps/registration_token.h"
+#include "mediapipe/framework/port/canonical_errors.h"
+#include "mediapipe/framework/port/statusor.h"
+
+#endif //MEDIAPIPE_DEPS_REGISTRATION_H_
